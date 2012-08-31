374bc39b46ef2187e34cc5d47a7ee5124c730fb5
prasanthj <j.prasanth.j@gmail.com>
2012-08-31 02:08:58 -0400

    Hello world of prolog

---
+++ README.md
@@ -0,0 +1,1 @@
+I am learning prolog (just for fun :)) and I am expecting to add more examples here.
---
+++ family-relations/family.pl
@@ -0,0 +1,10 @@
+%Foother is the brother of Booster
+brother(foother,booster).
+%Booster is the sister of Foother
+sister(booster,foother).
+%Foother is a boy and Booster is a girl
+gender(foother,male).
+gender(booster,female).
+%Foother and Booster are children of the family
+family(foother,child).
+family(booster,child).
